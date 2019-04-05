14a7ad11eaeebea4184afd6a626ff9f4899fe7af
Anthony Medina <antmedina91@gmail.com>
2019-04-05 10:43:08 +0100

    add user and topic to test data that have no relations

--- test-data/topics.js
+++ test-data/topics.js
@@ -7,4 +7,8 @@ module.exports = [
     description: 'Not dogs',
     slug: 'cats',
   },
+  {
+    description: 'what books are made of',
+    slug: 'paper',
+  },
 ];
--- test-data/users.js
+++ test-data/users.js
@@ -14,4 +14,10 @@ module.exports = [
     name: 'paul',
     avatar_url: 'https://avatars2.githubusercontent.com/u/24394918?s=400&v=4',
   },
+  {
+    username: 'lurker',
+    name: 'do_nothing',
+    avatar_url:
+      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+  },
 ];
