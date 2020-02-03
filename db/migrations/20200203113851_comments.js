44b74c4442693049f71c2e8b677f0557b9cb40cc
katieraby <katie.raby@icloud.com>
2020-02-03 15:53:52 +0000

    Completed comments migration

--- db/migrations/20200203113851_comments.js
+++ db/migrations/20200203113851_comments.js
@@ -1,5 +1,23 @@
 exports.up = function(knex) {
   console.log("creating comments table");
+  return knex.schema.createTable("comments", commentsTable => {
+    commentsTable.increments("comment_id").primary();
+    commentsTable
+      .string("author")
+      .references("username")
+      .inTable("users")
+      .notNullable();
+    commentsTable
+      .integer("article_id")
+      .references("article_id")
+      .inTable("articles")
+      .notNullable();
+    commentsTable
+      .integer("votes")
+      .defaultTo(0)
+      .notNullable();
+    commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex) {
