ecfcae8b647b6add5ccb6f2c8b0e3672adfc93b4
katieraby <katie.raby@icloud.com>
2020-02-04 15:23:52 +0000

    Minor edit to order of error handlers

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const apiRouter = require("./routers/apiRouter");
 const { customErrorHandler, pSQLErrorHandling } = require("./errors");
 app.use(express.json());
 
+app.use("/api", apiRouter);
+
 app.use(customErrorHandler);
 app.use(pSQLErrorHandling);
-app.use("/api", apiRouter);
 
 module.exports = app;
