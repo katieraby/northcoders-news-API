37fc788dca7c951fbd53ec4ebbeb509b155f74fd
katieraby <katie.raby@icloud.com>
2020-02-04 09:19:13 +0000

    Refactored formatDates function with tests, and added formatComments function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -3,20 +3,35 @@ returns new array.
 Each item in array has its timestamp converted to a javascript date object */
 exports.formatDates = list => {
   let newArray = list.map(obj => {
-    let newDate = new Date(obj["created_at"]).toUTCString();
+    let newDate = new Date(obj["created_at"]);
     return { ...obj, ["created_at"]: newDate };
   });
   return newArray;
 };
 
-exports.makeRefObj = (list, title, article_id) => {
+exports.makeRefObj = list => {
   const refObj = {};
   list.forEach(item => {
-    const key = item[title];
-    const val = item[article_id];
+    const key = item["title"];
+    const val = item["article_id"];
     refObj[key] = val;
   });
   return refObj;
 };
 
-exports.formatComments = (comments, articleRef) => {};
+// take an array of comment objects(`comments`) and a reference object, and return a new array of formatted comments.
+
+exports.formatComments = (comments, articleRef) => {
+  const formattedComments = comments.map(object => {
+    const newObj = {
+      ...object,
+      ["article_id"]: object["belongs_to"],
+      ["author"]: object["created_by"]
+    };
+    newObj["article_id"] = articleRef[newObj["article_id"]];
+    delete newObj["belongs_to"];
+    delete newObj["created_by"];
+    return newObj;
+  });
+  return this.formatDates(formattedComments);
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -5,12 +5,22 @@ const {
   formatComments
 } = require("../db/utils/utils");
 
-describe.only("formatDates", () => {
+describe("formatDates", () => {
   it("when passed an empty array, returns an empty array", () => {
     expect(formatDates([])).to.eql([]);
   });
 
-  it("when passed an array with one object containing a created_at key, converts the milliseconds to date format", () => {
+  it("when passed an array with one object, doesn't mutate the original array", () => {
+    const list = [
+      {
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    expect(formatDates(list)).to.not.equal(list);
+  });
+
+  it("the output object in the array has a different reference to the original object in the array", () => {
     const list = [
       {
         title: "Living in the shadow of a great man",
@@ -21,8 +31,21 @@ describe.only("formatDates", () => {
         votes: 100
       }
     ];
-    const output = formatDates(list);
-    expect(output[0]["created_at"]).to.equal("Thu, 15 Nov 2018 12:21:54 GMT");
+    expect(formatDates(list)[0]).to.not.equal(list[0]);
+  });
+
+  it("when passed an array with one object containing a created_at key, converts the milliseconds to date format", () => {
+    const list = [
+      {
+        created_at: 1542284514171
+      }
+    ];
+    const output = [
+      {
+        created_at: new Date(1542284514171)
+      }
+    ];
+    expect(formatDates(list)).to.eql(output);
   });
 
   it("when passed an array with multiple objects, converts the created_at value from milliseconds to date format", () => {
@@ -46,8 +69,8 @@ describe.only("formatDates", () => {
     ];
 
     const output = formatDates(list);
-    expect(output[0]["created_at"]).to.equal("Thu, 15 Nov 2018 12:21:54 GMT");
-    expect(output[1]["created_at"]).to.equal("Sun, 21 Oct 2018 01:15:18 GMT");
+    expect(output[0]["created_at"]).to.eql(new Date(1542284514171));
+    expect(output[1]["created_at"]).to.eql(new Date(1540084518901));
   });
 });
 
@@ -56,10 +79,15 @@ describe("makeRefObj", () => {
     expect(makeRefObj([])).to.eql({});
   });
 
+  it("the output object has a different reference to the original object in the array", () => {
+    const input = [{ article_id: 1, title: "A" }];
+    expect(makeRefObj(input)[0]).to.not.equal(input[0]);
+  });
+
   it("when passed an array with one object, returns an object with the title and ID key value pair", () => {
     const input = [{ article_id: 1, title: "A" }];
     const expected = { A: 1 };
-    expect(makeRefObj(input, "title", "article_id")).to.eql(expected);
+    expect(makeRefObj(input)).to.eql(expected);
   });
 
   it("when passed an array with two objects, returns an object with the titles as keys and the ID as values", () => {
@@ -68,7 +96,7 @@ describe("makeRefObj", () => {
       { article_id: 2, title: "D" }
     ];
     const expected = { A: 1, D: 2 };
-    expect(makeRefObj(input, "title", "article_id")).to.eql(expected);
+    expect(makeRefObj(input)).to.eql(expected);
   });
 
   it("when passed an array with multiple objects which have additional keys, returns an object with just the title-ID key value pair", () => {
@@ -78,8 +106,107 @@ describe("makeRefObj", () => {
       { article_id: 3, title: "E", otherKey: 14 }
     ];
     const expected = { A: 1, D: 2, E: 3 };
-    expect(makeRefObj(input, "title", "article_id")).to.eql(expected);
+    expect(makeRefObj(input)).to.eql(expected);
   });
 });
 
-describe("formatComments", () => {});
+describe("formatComments", () => {
+  it("when passed an empty array, returns an empty array", () => {
+    expect(formatComments([])).to.eql([]);
+  });
+
+  it("when passed an array with one object, doesn't mutate the original array", () => {
+    const comment = [
+      {
+        body:
+          "Corporis magnam placeat quia nulla illum nisi. Provident magni aut et earum illo labore aperiam. Dolorem ipsum dignissimos est ex. Minima voluptatibus nihil commodi veritatis. Magnam aut suscipit dignissimos nostrum ea.",
+        belongs_to: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+        created_by: "weegembump",
+        votes: 3,
+        created_at: 1504946266488
+      }
+    ];
+    const articleRefObj = {
+      "Running a Node App": 1,
+      "The Rise Of Thinking Machines: How IBM's Watson Takes On The World": 2,
+      "22 Amazing open source React projects": 3,
+      "A BRIEF HISTORY OF FOOD—NO BIG DEAL": 29
+    };
+    expect(formatComments(comment, articleRefObj)).to.not.equal(comment);
+  });
+
+  it("when passed one comment in an array of objects, and an article reference object, changes the created_by key to author, the belongs_to key to article_id key, and the created_at converted to a Javascript date object", () => {
+    const articleRefObj = {
+      "Running a Node App": 1,
+      "The Rise Of Thinking Machines: How IBM's Watson Takes On The World": 2,
+      "22 Amazing open source React projects": 3,
+      "A BRIEF HISTORY OF FOOD—NO BIG DEAL": 29
+    };
+    const comment = [
+      {
+        body:
+          "Corporis magnam placeat quia nulla illum nisi. Provident magni aut et earum illo labore aperiam. Dolorem ipsum dignissimos est ex. Minima voluptatibus nihil commodi veritatis. Magnam aut suscipit dignissimos nostrum ea.",
+        belongs_to: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+        created_by: "weegembump",
+        votes: 3,
+        created_at: 1504946266488
+      }
+    ];
+    const expected = [
+      {
+        body:
+          "Corporis magnam placeat quia nulla illum nisi. Provident magni aut et earum illo labore aperiam. Dolorem ipsum dignissimos est ex. Minima voluptatibus nihil commodi veritatis. Magnam aut suscipit dignissimos nostrum ea.",
+        article_id: 29,
+        author: "weegembump",
+        votes: 3,
+        created_at: new Date(1504946266488)
+      }
+    ];
+    const output = formatComments(comment, articleRefObj);
+
+    expect(formatComments(comment, articleRefObj)).to.eql(expected);
+    expect(output[0]).to.have.all.keys(
+      "body",
+      "article_id",
+      "author",
+      "votes",
+      "created_at"
+    );
+  });
+
+  it("when passed multiple comments in an array, changes the relevant properties as previous", () => {
+    const comments = [
+      {
+        body:
+          "Corporis magnam placeat quia nulla illum nisi. Provident magni aut et earum illo labore aperiam. Dolorem ipsum dignissimos est ex. Minima voluptatibus nihil commodi veritatis. Magnam aut suscipit dignissimos nostrum ea.",
+        belongs_to: "A BRIEF HISTORY OF FOOD—NO BIG DEAL",
+        created_by: "weegembump",
+        votes: 3,
+        created_at: 1504946266488
+      },
+      {
+        body:
+          "Reiciendis enim soluta a sed cumque dolor quia quod sint. Laborum tempore est et quisquam dolore. Qui voluptas consequatur cumque neque et laborum unde sed. Impedit et consequatur tempore dignissimos earum distinctio cupiditate.",
+        belongs_to: "Who are the most followed clubs and players on Instagram?",
+        created_by: "happyamy2016",
+        votes: 17,
+        created_at: 1489789669732
+      }
+    ];
+    const articleRefObj = {
+      "Running a Node App": 1,
+      "The Rise Of Thinking Machines: How IBM's Watson Takes On The World": 2,
+      "22 Amazing open source React projects": 3,
+      "A BRIEF HISTORY OF FOOD—NO BIG DEAL": 29,
+      "Who are the most followed clubs and players on Instagram?": 14
+    };
+    const output = formatComments(comments, articleRefObj);
+    expect(output[1]).to.have.all.keys(
+      "body",
+      "article_id",
+      "author",
+      "votes",
+      "created_at"
+    );
+  });
+});
