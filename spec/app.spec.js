51486bb146fcaf58198ee1f37509e805ff189522
katieraby <katie.raby@icloud.com>
2020-02-04 13:56:55 +0000

    Completed test gor GET request to users/:username endpoint

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -18,13 +18,28 @@ describe("/api", () => {
           expect(body.topics[0]).to.have.all.keys("slug", "description");
         });
     });
-    it("GET - returns a status 404 - not found, and a message, when the user tries to access an unavailable route", () => {
-      return request(app)
-        .get("/api/topics/cheese")
-        .expect(404)
-        .then(({ err }) => {
-          expect(err.msg).to.equal("Not found");
-        });
+    // it("GET - returns a status 404 - not found, and a message, when the user tries to access an unavailable route", () => {
+    //   return request(app)
+    //     .get("/api/topics/cheese")
+    //     .expect(404)
+    //     .then(({ err }) => {
+    //       expect(err.msg).to.equal("Not found");
+    //     });
+    // });
+  });
+
+  describe.only("/users", () => {
+    describe("/:username", () => {
+      it("GET returns a status 200 and a user object when parametric username endpoint is accessed", () => {
+        return request(app)
+          .get("/api/users/rogersop")
+          .expect(200)
+          .then(({ body }) => {
+            console.log(body, "bodylog");
+            expect(body.username).to.equal("rogersop");
+            expect(body).to.have.all.keys("username", "avatar_url", "name");
+          });
+      });
     });
   });
 });
