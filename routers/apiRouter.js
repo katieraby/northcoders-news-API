8232504dd00b850a2f75e9e96610a908980d0fc3
Katie Raby <katie.raby@icloud.com>
2020-02-24 15:49:15 +0000

    Added route get request for API

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -3,10 +3,14 @@ const topicsRouter = require("../routers/topicsRouter");
 const usersRouter = require("../routers/usersRouter");
 const articlesRouter = require("../routers/articlesRouter");
 const commentsRouter = require("../routers/commentsRouter");
+const { getAllEndpoints } = require("../controllers/apiController");
 
 const { send405Error } = require("../errors");
 
-apiRouter.route("/").all(send405Error);
+apiRouter
+  .route("/")
+  .get(getAllEndpoints)
+  .all(send405Error);
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
