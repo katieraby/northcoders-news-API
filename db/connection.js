1f03107481457f7020619606a2380e695e449da9
Katie Raby <katie.raby@icloud.com>
2020-02-24 16:22:39 +0000

    Preparing files for connection to hosted database when on Heroku

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,9 @@
+const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
-const dbConfig = require("../knexfile");
+
+const dbConfig =
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
 
 module.exports = knex(dbConfig);
--- db/data/index.js
+++ db/data/index.js
@@ -4,7 +4,8 @@ const devData = require("./development-data");
 
 const data = {
   development: devData,
-  test: testData
+  test: testData,
+  production: devData
 };
 
 module.exports = data[ENV];
